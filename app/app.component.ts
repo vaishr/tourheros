bb1428bde868df8361e4feda6fc03955623ec621
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-06-28 23:49:10 -0700

    create hero service and separate data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,8 @@
 import {Component} from '@angular/core';
+import { OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -80,12 +68,20 @@ const HEROES: Hero[] = [
         border-radius: 4px 0 0 4px;
        }
      `],
-     directives: [HeroDetailComponent]
+     directives: [HeroDetailComponent],
+     providers: [HeroService]
 })
 
-export class AppComponent { 
+export class AppComponent implements OnInit { 
   title = "Tour of Heros";
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
